e572e6cf12030c97694227d376218fa2d6b8d9cf
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-11 00:50:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_to_negativize(source_array) 
+  index = 0 
+  return_array = []
+  while index < source_array.length 
+    return_array << source_array[index] * -1
+    index += 1 
+  end
+  return_array
+end 
