199a312f96dcc3845d09ab7fede1c2b23366b248
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-11 01:04:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,12 +38,8 @@ def reduce_to_total(source_array, starting_point = 0)
   index = 0 
   total = 0 
   while index < source_array.length 
-    if(starting_point == 0)
-      total += source_array[index]
-    else 
-      total = total + starting_point + source_array[index]
-    end 
+    total += source_array[index]
     index += 1  
   end
-  total
+  total += starting_point
 end
